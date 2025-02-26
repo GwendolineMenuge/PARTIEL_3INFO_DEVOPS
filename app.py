ab26ef6f4a5b6b2ef91e3a7d254d7c6075539e3d
GwendolineMenuge <menuge.gwendoline@gmail.com>
2025-02-26 12:08:21 +0100

    modif

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
 def add(a, b):
     """Ajoute deux nombres."""
-    return a + b
+    return a+b
 
 def multiply(x, y):
-    """Multiplie deux nombres."""
-    return x * y
+    return x*y
 
-def divide(x, y):
-    """Divise x par y en évitant la division par zéro."""
+def divide(x , y):
+    beta = 0
     if y != 0:
-        return x / y
-    return None  # Bonne pratique : retourner une valeur explicite en cas d'erreur
+        return x/y
 
 def greet(name):
-    """Renvoie un message de salutation."""
-    if not name:  # Vérifie si name est vide ou None
+    # GREET FONCTION
+    if name =="":
         return "Hello, World!"
-    return f"Hello, {name}"
+        return f"Hello,"+ [name]
